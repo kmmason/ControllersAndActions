c3507f4de85ea02a596ef0abf9fcdde9c21df7bd
Kevin Mason <kevin.mason@outlook.com>
2017-08-18 13:44:54 -0500

    ControllerAndActions Creating a Controller by Deriving from the Controller Class

---
+++ ControllerAandActions/Controllers/DerivedController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ControllerAandActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+    }
+}
