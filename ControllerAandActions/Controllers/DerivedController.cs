2f9786072200856d4e152c504e5847e2d7ffaf28
Kevin Mason <kevin.mason@outlook.com>
2017-08-18 14:07:35 -0500

    ControllersAndActions Getting Data from Context Objects

--- ControllerAandActions/Controllers/DerivedController.cs
+++ ControllerAandActions/Controllers/DerivedController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace ControllerAandActions.Controllers
 {
@@ -8,6 +9,25 @@ namespace ControllerAandActions.Controllers
         {
             ViewBag.Message = "Hello from the DerivedController Index method";
             return View("MyView");
+        }
+
+        public ActionResult RenameProduct()
+        {
+            //Access various properties from context objects
+            string userName = User.Identity.Name;
+            string serverName = Server.MachineName;
+            string clientIP = Request.UserHostAddress;
+            DateTime dateStamp = HttpContext.Timestamp;
+
+            AuditRequest(userName, serverName, clientIP, dateStamp, "Renaming Product");
+
+            //Retrieve posted data from Request.Form
+            string oldProductName = Request.Form["OldName"];
+            string newProductName = Request.Form["NewName"];
+
+            bool result = AttemptProductRename(oldProductName, newProductName);
+            ViewData["RenameResult"] = result;
+            return View("ProductRenamed");
         }
     }
 }
